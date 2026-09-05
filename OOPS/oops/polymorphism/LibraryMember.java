bd543106a956c1b0811d5d77c87c5b63f6e28a4d
muskansharma30 <muskan.sharma_cs23@gla.ac.in>
2026-09-05 15:00:46 +0530

    commit

--- OOPS/oops/polymorphism/LibraryMember.java
+++ OOPS/oops/polymorphism/LibraryMember.java
@@ -1,4 +1,109 @@
 package oops.polymorphism;
+import java.util.Scanner;
 
+// Superclass
 public class LibraryMember {
+    String memberName;
+    String memberId;
+
+    LibraryMember(String memberName, String memberId) {
+        this.memberName = memberName;
+        this.memberId = memberId;
+    }
+
+    double calculateFine(int overdueDays) {
+        return 0;
+    }
+
+    void displayDetails(int overdueDays) {
+        System.out.println("Member Name: " + memberName);
+        System.out.println("Member ID: " + memberId);
+        System.out.println("Fine: ₹" + calculateFine(overdueDays));
+    }
+}
+
+// Student Member
+class StudentMember extends LibraryMember {
+
+    StudentMember(String name, String id) {
+        super(name, id);
+    }
+
+    @Override
+    double calculateFine(int overdueDays) {
+        return overdueDays * 2; // ₹2/day
+    }
+}
+
+// Faculty Member
+class FacultyMember extends LibraryMember {
+
+    FacultyMember(String name, String id) {
+        super(name, id);
+    }
+
+    @Override
+    double calculateFine(int overdueDays) {
+        return overdueDays * 1; // ₹1/day
+    }
+}
+
+// Guest Member
+class GuestMember extends LibraryMember {
+
+    GuestMember(String name, String id) {
+        super(name, id);
+    }
+
+    @Override
+    double calculateFine(int overdueDays) {
+        return overdueDays * 5; // ₹5/day
+    }
+}
+
+// Main Class
+    class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        LibraryMember[] members = new LibraryMember[3];
+
+        // Store members
+        members[0] = new StudentMember("Muskan", "S101");
+        members[1] = new FacultyMember("Riya", "F102");
+        members[2] = new GuestMember("Aman", "G103");
+
+        System.out.print("Enter overdue days: ");
+        int overdueDays = sc.nextInt();
+        sc.nextLine();
+
+        // Display all members
+        System.out.println("\nMember Details:");
+        for (LibraryMember m : members) {
+            m.displayDetails(overdueDays);
+            System.out.println();
+        }
+
+        // Search by ID
+        System.out.print("Enter Member ID to search: ");
+        String searchId = sc.nextLine();
+
+        boolean found = false;
+
+        for (LibraryMember m : members) {
+            if (m.memberId.equalsIgnoreCase(searchId)) {
+                System.out.println("\nMember Found:");
+                m.displayDetails(overdueDays);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Member ID not found.");
+        }
+
+        sc.close();
+    }
 }
--- OOPS/oops/polymorphism/Vehicle.java
+++ OOPS/oops/polymorphism/Vehicle.java
@@ -1,4 +1,70 @@
 package oops.polymorphism;
 
 public class Vehicle {
+
+    double fuelCost(int km) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        Vehicle[] vehicles = {
+                new Car(),
+                new Bus(),
+                new Bike(),
+                new ElectricCar()
+        };
+
+        int km = 100;
+
+        for (Vehicle v : vehicles) {
+
+            System.out.println("Vehicle: " + v.getClass().getSimpleName());
+            System.out.println("Fuel Cost: " + v.fuelCost(km));
+
+            if (v instanceof Car) {
+                System.out.println("This is a Car");
+            } else if (v instanceof Bus) {
+                System.out.println("This is a Bus");
+            } else if (v instanceof Bike) {
+                System.out.println("This is a Bike");
+            } else if (v instanceof ElectricCar) {
+                System.out.println("This is an Electric Car");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+class Car extends Vehicle {
+
+    @Override
+    double fuelCost(int km) {
+        return km * 8;
+    }
+}
+
+class Bus extends Vehicle {
+
+    @Override
+    double fuelCost(int km) {
+        return km * 15;
+    }
+}
+
+class Bike extends Vehicle {
+
+    @Override
+    double fuelCost(int km) {
+        return km * 3;
+    }
+}
+
+class ElectricCar extends Vehicle {
+
+    @Override
+    double fuelCost(int km) {
+        return km * 2;
+    }
 }
